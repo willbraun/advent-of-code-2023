ba0cc333a17b7fe2f6a68d4b7fd7e02bef104104
willbraun <williamhbraun1@gmail.com>
2023-12-01 14:54:44 -0500

    completed day 1

---
+++ day_01/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	part1()
+	part2()
+}
+
+// find the sum of the two-digit numbers made from the first and last digit on each line
+func part1() {
+	dataPath, err := filepath.Abs("./data.txt")
+	if err != nil {
+		log.Fatal("Error getting current directory:", err)
+		return
+	}
+
+	data, err := os.ReadFile(dataPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	text := string(data)
+	lines := strings.Split(text, "\n")
+	sum := 0
+
+	for _, line := range lines {
+		chars := strings.Split(line, "")
+		numChars := []string{}
+		for _, char := range chars {
+			_, err := strconv.Atoi(char)
+			if err == nil {
+				numChars = append(numChars, char)
+			}
+		}
+
+		digit1 := numChars[0]
+		digit2 := numChars[len(numChars)-1]
+		numStr := digit1 + digit2
+
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		sum += num
+	}
+
+	fmt.Println("Part 1:", sum)
+}
+
+// same as part 1, but include the spelled out numbers as digits
+func part2() {
+	dataPath, err := filepath.Abs("./data.txt")
+	if err != nil {
+		log.Fatal("Error getting current directory:", err)
+		return
+	}
+
+	data, err := os.ReadFile(dataPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	text := string(data)
+	lines := strings.Split(text, "\n")
+	sum := 0
+
+	numberMap := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+
+	for _, line := range lines {
+		indexMap := make(map[int]string)
+		for word, num := range numberMap {
+			index := strings.Index(line, word)
+			if index != -1 {
+				indexMap[index] = num
+			}
+
+			lastIndex := strings.LastIndex(line, word)
+			if lastIndex != -1 {
+				indexMap[lastIndex] = num
+			}
+		}
+
+		chars := strings.Split(line, "")
+		for index, char := range chars {
+			_, err := strconv.Atoi(char)
+			if err == nil {
+				indexMap[index] = char
+			}
+		}
+
+		min := 1000
+		max := 0
+		for key := range indexMap {
+			if key < min {
+				min = key
+			}
+			if key > max {
+				max = key
+			}
+		}
+
+		digit1 := indexMap[min]
+		digit2 := indexMap[max]
+		numStr := digit1 + digit2
+
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		sum += num
+	}
+
+	fmt.Println("Part 2:", sum)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module day_01_1/main
+
+go 1.21.1

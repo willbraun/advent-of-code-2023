df08482825959d93595187a27595b005786df04d
willbraun <williamhbraun1@gmail.com>
2023-12-02 18:10:23 -0500

    completed day 2

---
+++ day_02/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	part1()
+	part2()
+}
+
+func part1() {
+	dataPath, err := filepath.Abs("./data.txt")
+	if err != nil {
+		log.Fatal("Error getting current directory:", err)
+		return
+	}
+
+	data, err := os.ReadFile(dataPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	text := string(data)
+	games := strings.Split(text, "\n")
+	sum := 0
+
+	colorMap := map[string]int{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
+
+games:
+	for _, game := range games {
+		data := strings.Split(game, ":")
+		gameNum, err := strconv.Atoi(strings.Split(data[0], " ")[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		gameData := data[1]
+
+		rounds := strings.Split(gameData, ";")
+		for _, round := range rounds {
+			numColorPairs := strings.Split(round, ",")
+			for _, numColorPair := range numColorPairs {
+				numColorSlice := strings.Split(strings.Trim(numColorPair, " "), " ")
+				numStr := numColorSlice[0]
+				color := numColorSlice[1]
+
+				num, err := strconv.Atoi(numStr)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if num > colorMap[color] {
+					continue games
+				}
+			}
+		}
+		sum += gameNum
+	}
+
+	fmt.Println("Part 1:", sum)
+}
+
+func part2() {
+	dataPath, err := filepath.Abs("./data.txt")
+	if err != nil {
+		log.Fatal("Error getting current directory:", err)
+		return
+	}
+
+	data, err := os.ReadFile(dataPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	text := string(data)
+	games := strings.Split(text, "\n")
+	sum := 0
+
+	for _, game := range games {
+		gameData := strings.Split(game, ":")[1]
+		rounds := strings.Split(gameData, ";")
+		maximums := make(map[string]int)
+
+		for _, round := range rounds {
+			numColorPairs := strings.Split(round, ",")
+			for _, numColorPair := range numColorPairs {
+				numColorSlice := strings.Split(strings.Trim(numColorPair, " "), " ")
+				numStr := numColorSlice[0]
+				color := numColorSlice[1]
+
+				num, err := strconv.Atoi(numStr)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				max, ok := maximums[color]
+				if !ok || num > max {
+					maximums[color] = num
+				}
+			}
+		}
+
+		power := maximums["red"] * maximums["green"] * maximums["blue"]
+		sum += power
+	}
+
+	fmt.Println("Part 2:", sum)
+}

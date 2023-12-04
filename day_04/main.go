03b59cc14520a464545753b469c11a6d73ceea8d
willbraun <williamhbraun1@gmail.com>
2023-12-04 16:46:37 -0500

    completed day 4

--- day_04/main.go
+++ day_04/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -25,12 +26,20 @@ func main() {
 	text := string(data)
 	lines := strings.Split(text, "\n")
 	sum := float64(0)
+	lineCounts := make(map[int]int)
 
 	for _, line := range lines {
-		splitLine := strings.Split(line, "|")
-		winString := strings.Split(splitLine[0], ":")[1]
-		winningNums := regexp.MustCompile(`\d+`).FindAllString(winString, -1)
-		myNums := regexp.MustCompile(`\d+`).FindAllString(splitLine[1], -1)
+		splitPipe := strings.Split(line, "|")
+		splitColon := strings.Split(splitPipe[0], ":")
+		cardNum, err := strconv.Atoi(regexp.MustCompile(`\d+`).FindString(splitColon[0]))
+		if err != nil {
+			log.Fatal(err)
+		}
+		// add original card
+		lineCounts[cardNum]++
+
+		winningNums := regexp.MustCompile(`\d+`).FindAllString(splitColon[1], -1)
+		myNums := regexp.MustCompile(`\d+`).FindAllString(splitPipe[1], -1)
 
 		wins := float64(0)
 		for _, w := range winningNums {
@@ -46,7 +55,22 @@ func main() {
 		}
 
 		sum += math.Pow(2, wins-1)
+
+		// add one copy of next i cards, where i is number of wins
+		for i := 0; i < int(wins); i++ {
+			// do this for each copy of your current card
+			for j := 0; j < lineCounts[cardNum]; j++ {
+				lineCounts[cardNum+1+i]++
+			}
+		}
 	}
 
 	fmt.Println("Part 1:", sum)
+
+	totalCount := 0
+	for _, count := range lineCounts {
+		totalCount += count
+	}
+
+	fmt.Println("Part 2:", totalCount)
 }

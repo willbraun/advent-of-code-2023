c28eeeab3eb471319b836444a1d2b630b3ed92d1
willbraun <williamhbraun1@gmail.com>
2023-12-05 17:29:20 -0500

    working on day 5 part 2

--- day_05/main.go
+++ day_05/main.go
@@ -47,15 +47,55 @@ func main() {
 	// in progress
 	// Part 2 - seed numbers come in pairs, the start number and the range. Calculate again with new list of seeds.
 
-	// seedNumsPart2 := []int{}
+	// closestLocation := math.Inf(1)
+
 	// for i := 0; i < len(seedNums); i+= 2 {
 	// 	fmt.Println(seedNums[i])
 	// 	for j := 0; j < seedNums[i+1]; j++ {
-	// 		seedNumsPart2 = append(seedNumsPart2, seedNums[i] + j)
+	// 		currSeed := seedNums[i] + j
+	// 		location := seedToLocation(currSeed, sections)
+	// 		location64 := float64(location)
+	// 		if location64 < closestLocation {
+	// 			closestLocation = location64
+	// 		}
 	// 	}
 	// }
 
-	// fmt.Println("Part 2:", seedsToLocation(seedNumsPart2, sections))
+	// fmt.Println("Part 2:", closestLocation)
+}
+
+func seedToLocation(seed int, sections []string) int {
+	mapResult := seed
+	sections:
+		for _, section := range sections[1:] {
+			lines := strings.Split(section, "\n")
+			for _, line := range lines[2:] {
+				rowDataStrings := regexp.MustCompile(`\d+`).FindAllString(line, -1)
+				if len(rowDataStrings) == 0 {
+					continue
+				} else {
+					rowDataNums := []int{}
+					for _, str := range rowDataStrings {
+						num, err := strconv.Atoi(str)
+						if err != nil {
+							log.Fatal(err)
+						}
+						rowDataNums = append(rowDataNums, num)
+					}
+
+					destination := rowDataNums[0]
+					source := rowDataNums[1]
+					dataRange := rowDataNums[2]
+
+					if (mapResult >= source) && (mapResult < (source + dataRange)) {
+						diff := mapResult - source
+						mapResult = destination + diff
+						continue sections
+					}
+				}
+			}
+		}
+	return mapResult
 }
 
 func seedsToLocation(seeds []int, sections []string) int {
